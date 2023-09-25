c56e12da60e308c96de734e6db3d2d8fdc424ad3
HarveyXpert <116091259+HarveyXpert@users.noreply.github.com>
2023-09-25 13:51:45 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php
+  echo 'Hello World. I am Dairin.';
+
+  $value1 = 10;
+  $value2 = 50;
+
+  $sum = $value1 + $value2;
+
+  echo '<br>';
+  echo 'The sum of' .$value1. ' and '. $value2. ' is '.$sum;
+  ?>
